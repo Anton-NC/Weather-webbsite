1028851eec7b4bdda1759067970c0a9a0ba40bde
Anton-NC <105290862+Anton-NC@users.noreply.github.com>
2022-12-31 17:32:16 +0100

    change 2

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-
-
-
 //deklarerar id för javascripten för att linka htmlen
 var inputval = document.querySelector('#cityinput')
 var btn = document.querySelector('#add');
@@ -8,6 +5,8 @@ var city = document.querySelector('#cityoutput')
 var descrip = document.querySelector('#description')
 var temp = document.querySelector('#temp')
 var wind = document.querySelector('#wind')
+
+
 
 //Kelving till celsius ungefär -273f för att få det till celsius
 
@@ -18,13 +17,9 @@ function convertion(val){
     btn.addEventListener('click', function(){
 
 // från api länken får vi informationen
-
-
-
         fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputval.value +'&appid=85423b7b041d4bf759ac00d1684704b4')
         .then(res => res.json())
         .then(data => {
-
     //förvarar värderna
 
             var nameval = data['name']
@@ -43,8 +38,47 @@ function convertion(val){
     //error mess om du typar in fel namn och om du trycker på knappen utan att skriva något.
         .catch(err => alert('You entered Wrong city name'))
     }
-
-    // Initialize and add the map
 )
 
-    
+//vill ha användarens lokations data för att visa lokala positionen
+
+//checka om api:ns geolocation finns
+if(navigator.geolocation) {
+    //om det stämmer
+    alert("Location");
+} else {
+    alert('geolocation NOT available');
+}
+//callback
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(pos){
+        var geoLat = pos.coords.latitude.toFixed(5);
+        var geoLng = pos.coords.longitude.toFixed(5);
+        var geoAcc = pos.coords.accuracy.toFixed(1);
+
+    });
+}
+//Getcurrentposition error returneras
+function getPosErr(err) {
+    switch (err.code){
+        case err.PERMISSION_DENIED:
+            alert("User have denied the geolocation")
+            break;
+        case err.POSITION_UNAVAILABLE:
+            alert("Location information is unavailable")
+            break;
+        case err.TIMEOUT:
+            alert("Location timed out");
+            break;
+            default:
+                alert("Unknown error");
+    }
+}
+
+
+
+
+
+
+
+
